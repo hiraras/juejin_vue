6880799ae444af150e92471c730ac37aa418b907
hiraras <kazilagui@163.com>
2020-04-01 10:41:24 +0800

    新项目

---
+++ config/index.js
@@ -0,0 +1,24 @@
+
+
+const com = {
+  IP: JSON.stringify('xxx')
+};
+
+module.exports = {
+
+  // 开发环境变量
+  dev: {
+    env: {
+          TYPE: JSON.stringify('dev'),
+          ...com
+    }
+  },
+  
+  // 生产环境变量
+  build: {
+    env: {
+          TYPE: JSON.stringify('prod'),
+          ...com
+    }
+  }
+}
